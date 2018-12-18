2370c45d44e0f9b0a2ce10524dd1b76b473fc6c6
Ben Hakes <bhhakes@gmail.com>
2018-12-18 15:23:08 -0600

    add swipe to delete feat.

--- Tasks/View Controllers/TasksDetailViewController.swift
+++ Tasks/View Controllers/TasksDetailViewController.swift
@@ -12,7 +12,8 @@ class TasksDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateViews()
         // Do any additional setup after loading the view.
     }
     @IBOutlet weak var nameLabel: UILabel!
@@ -20,8 +21,44 @@ class TasksDetailViewController: UIViewController {
     @IBOutlet weak var notesLabel: UILabel!
     @IBOutlet weak var notesTextView: UITextView!
     
+    private func updateViews(){
+        guard isViewLoaded else {return}
+        
+        title = task?.name ?? "Create Task"
+        nameTextField.text = task?.name
+        notesTextView.text = task?.notes
+    }
+    
+    var task: Task? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBAction func save(_ sender: Any) {
         
+        guard let name = nameTextField.text, !name.isEmpty else {
+            return
+        }
+        let notes = notesTextView.text
+        
+        if let task = task {
+            //Editing existing task
+            task.name = name
+            task.notes = notes
+        } else {
+            // Create a new task
+            _ = Task(name: name, notes: notes)
+        }
+        
+        let moc = CoreDataStack.shared.mainContext
+        do {
+            try moc.save()
+        } catch{
+            NSLog("Error saving managed object context: \(error)")
+        }
+        
+        navigationController?.popViewController(animated: true)
     }
     
 }
--- Tasks/View Controllers/TasksTableViewController.swift
+++ Tasks/View Controllers/TasksTableViewController.swift
@@ -7,84 +7,76 @@
 //
 
 import UIKit
+import CoreData
 
 class TasksTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+    // this is a variable with a computed property
+    var tasks: [Task] {
+        
+        // we should really only fetch this when the data changes
+        // right now this is getting fetched every time
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let moc = CoreDataStack.shared.mainContext
+        do {
+            return try moc.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching tasks: \(error)")
+            return []
+        }
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tasks.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
 
+        let task = tasks[indexPath.row]
+        
         // Configure the cell...
+        cell.textLabel?.text = task.name
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "ShowDetail" {
+            let detailVC = segue.destination as! TasksDetailViewController
+            if let indexPath = tableView.indexPathForSelectedRow{
+                detailVC.task = tasks[indexPath.row]
+            }
+        }
+        
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        guard editingStyle == .delete else { return }
+        
+        let task = tasks[indexPath.row]
+        
+        let moc = CoreDataStack.shared.mainContext
+        
+        moc.delete(task)
+        tableView.reloadData()
+        
+    }
 
 }
